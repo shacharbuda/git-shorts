a8519275c1752d5eba4dfb37ac6f6e6abda16e9c
Shachar <shacbud@gmail.com>
2019-05-03 12:08:03 +0300

    remove  anything but simple aliases and comments

--- git.plugin.sh
+++ git.plugin.sh
@@ -1,38 +1,3 @@
-# Query/use custom command for `git`.
-zstyle -s ":vcs_info:git:*:-all-" "command" _omz_git_git_cmd
-: ${_omz_git_git_cmd:=git}
-
-#
-# Functions
-#
-
-# The name of the current branch
-# Back-compatibility wrapper for when this function was defined here in
-# the plugin, before being pulled in to core lib/git.zsh as git_current_branch()
-# to fix the core -> git plugin dependency.
-function current_branch() {
-  git_current_branch
-}
-# The list of remotes
-function current_repository() {
-  if ! $_omz_git_git_cmd rev-parse --is-inside-work-tree &> /dev/null; then
-    return
-  fi
-  echo $($_omz_git_git_cmd remote -v | cut -d':' -f 2)
-}
-# Pretty log messages
-function _git_log_prettily(){
-  if ! [ -z $1 ]; then
-    git log --pretty=$1
-  fi
-}
-# Warn if the current branch is a WIP
-function work_in_progress() {
-  if $(git log -n 1 2>/dev/null | grep -q -c "\-\-wip\-\-"); then
-    echo "WIP!!"
-  fi
-}
-
 #
 # Aliases
 # (sorted alphabetically)
@@ -85,7 +50,6 @@ alias gcmsg='git commit -m'
 alias gco='git checkout'
 alias gco-='git checkout -'
 alias gcount='git shortlog -sn'
-compdef _git gcount
 alias gcp='git cherry-pick'
 alias gcpa='git cherry-pick --abort'
 alias gcpc='git cherry-pick --continue'
@@ -99,72 +63,18 @@ alias gds='git diff --staged'
 alias gdt='git diff-tree --no-commit-id --name-only -r'
 alias gdw='git diff --word-diff'
 
-gdv() { git diff -w "$@" | view - }
-compdef _git gdv=git-diff
-
 alias gf='git fetch'
 alias gfa='git fetch --all --prune'
 alias gfo='git fetch origin'
 
-function gfg() { git ls-files | grep $@ }
-compdef _grep gfg
-
 alias gg='git gui citool'
 alias gga='git gui citool --amend'
 
-ggf() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git push --force origin "${b:=$1}"
-}
-ggfl() {
-[[ "$#" != 1 ]] && local b="$(git_current_branch)"
-git push --force-with-lease origin "${b:=$1}"
-}
-compdef _git ggf=git-checkout
-
-ggl() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git pull origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git pull origin "${b:=$1}"
-  fi
-}
-compdef _git ggl=git-checkout
-
-ggp() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git push origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git push origin "${b:=$1}"
-  fi
-}
-compdef _git ggp=git-checkout
-
-ggpnp() {
-  if [[ "$#" == 0 ]]; then
-    ggl && ggp
-  else
-    ggl "${*}" && ggp "${*}"
-  fi
-}
-compdef _git ggpnp=git-checkout
-
-ggu() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git pull --rebase origin "${b:=$1}"
-}
-compdef _git ggu=git-checkout
-
 alias ggpur='ggu'
-compdef _git ggpur=git-checkout
 
 alias ggpull='git pull origin "$(git_current_branch)"'
-compdef _git ggpull=git-checkout
 
 alias ggpush='git push origin "$(git_current_branch)"'
-compdef _git ggpush=git-checkout
 
 alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
 alias gpsup='git push --set-upstream origin $(git_current_branch)'
@@ -174,12 +84,9 @@ alias ghh='git help'
 alias gignore='git update-index --assume-unchanged'
 alias gignored='git ls-files -v | grep "^[[:lower:]]"'
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
-compdef _git git-svn-dcommit-push=git
 
 alias gk='\gitk --all --branches'
-compdef _git gk='gitk'
 alias gke='\gitk --all $(git log -g --pretty=%h)'
-compdef _git gke='gitk'
 
 alias gl='git pull'
 alias glg='git log --stat'
@@ -196,7 +103,6 @@ alias glola="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgr
 # alias glog='git log --oneline --decorate --graph'
 # alias gloga='git log --oneline --decorate --graph --all'
 alias glp="_git_log_prettily"
-compdef _git glp=git-log
 
 alias gm='git merge'
 alias gmom='git merge origin/master'
@@ -210,7 +116,6 @@ alias gpd='git push --dry-run'
 alias gpf='git push --force-with-lease'
 alias gpf!='git push --force'
 alias gpoat='git push origin --all && git push origin --tags'
-compdef _git gpoat=git-push
 alias gpu='git push upstream'
 alias gpv='git push -v'
 
